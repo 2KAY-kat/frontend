edadcf8ed351cd6337795d2ceab51919157124dd
2KAY-kat <dambwedesigns@gmail.com>
2024-12-23 13:34:07 +0200

    slowly progressing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Store from './Pages/Store';
+import StoreCategory from './Pages/StoreCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+
+  <BrowserRouter>
+    <Navbar/>
+
+    <Routes>
+      <Route path='/' element={<Store/>}/>
+      <Route path='/mens' element={<StoreCategory category="men"/>}/>
+      <Route path='/womens' element={<StoreCategory category="women"/>}/>
+      <Route path='/kids' element={<StoreCategory category="kid"/>}/>
+      <Route path="product" element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+
+  </BrowserRouter>
+      
     </div>
   );
 }
--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Store = () => {
 
 
     return (
         <div>
-            
+            <Hero />
+            <Popular />
+            <Offers />
         </div>
     )
 }
