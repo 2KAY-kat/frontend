1eb42d8d583bdc48a97852537367b661e4f763e5
2KAY-kat <dambwedesigns@gmail.com>
2025-02-04 22:51:21 +0200

    checkout cart styled

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -60,13 +60,18 @@ const CartItems = () => {
                     </div>
                     <button>PROCEED TO CHECKOUT</button>
                 </div>
-                <div className="cartitems-promocode">
-                    <p>If you have a prom coce enter it here</p>
+                {/* 
+                
+                // this the promo code input box
+
+                 <div className="cartitems-promocode">
+                    <p>If you have a promo code enter it here</p>
                     <div className="cartitems-promobox">
                         <input type="text" placeholder="promo code" />
                         <button>Submit</button>
-                    </div>
+                    </div> 
                 </div>
+                */}
             </div>
 
         </div>
